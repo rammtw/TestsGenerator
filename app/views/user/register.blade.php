c9cf5231e93dc53fe3e401661d0e697ffbdeeab2
rammtw <f0g16ramil@mail.ru>
2014-12-03 14:00:07 +0300

    прикручен шаблон к странице регистрации

--- app/views/user/register.blade.php
+++ app/views/user/register.blade.php
@@ -1,18 +1,49 @@
 <!DOCTYPE html>
-<html lang="en-US">
+<html lang="en">
 	<head>
 		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="">
+		<meta name="author" content="">
+
+		<title>Signin Template for Bootstrap</title>
+
+		<!-- Bootstrap core CSS -->
+		<link href="../../css/bootstrap.min.css" rel="stylesheet">
+
+		<!-- Custom styles for this template -->
+		<link href="../../css/signin.css" rel="stylesheet">
+
+		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
 	</head>
-	<body>
-		<h2>Register</h2>
-		<div>
-			{{ Form::open(array('action' => 'UserController@create')) }}
-  			{{ Form::text('name') }}
-  			{{ Form::text('last_name') }}
-  			{{ Form::password('password') }}
-    		{{ Form::select('group_id', $groups) }}
-  			{{ Form::submit('Зарегистрироваться') }}
-			{{ Form::close() }}
-		</div>
-	</body>
+
+  <body>
+
+    <div class="container">
+
+		{{ Form::open(array('action' => 'UserController@create', 'class' => 'form-signin')) }}
+			@if(Session::get('error'))
+				<p class="bg-danger" style="padding:15px;">Неверный логин или пароль</p>
+			@endif
+			<h2 class="form-signin-heading">Регистрация</h2>
+			<input type="hidden" name="_token" value="{{ csrf_token() }}">
+			{{ Form::select('group_id', $groups, null, array('class' => 'form-control input-sm')) }}
+			<input type="name" name="name" class="form-control" placeholder="Имя" required autofocus>
+			<input type="last_name" name="last_name" class="form-control" placeholder="Фамилия" required>
+			<input type="password" name="password" class="form-control" placeholder="Password" required>
+			<button class="btn btn-lg btn-success btn-block" type="submit">Зарегистрироваться</button>
+		{{ Form::close() }}
+
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+  </body>
 </html>
